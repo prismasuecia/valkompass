4497962f130afd42b6b65f75abfc6d7951660f49
Joel Lindhe <info@joellindhe.com>
2026-05-27 10:49:37 +0200

    Deploy app with GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
-  outputFileTracingRoot: process.cwd()
+  output: 'export',
+  outputFileTracingRoot: process.cwd(),
+  basePath: isGitHubActions ? '/valkompass' : '',
+  assetPrefix: isGitHubActions ? '/valkompass/' : ''
 };
 
 export default nextConfig;
